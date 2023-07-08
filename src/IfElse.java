fe9d2e5db7c41f8d60240574c7a469590411e0e8
Niki Patel <hitaiship1995@gmail.com>
2023-07-08 08:21:37 -0400

    First Repo

--- src/IfElse.java
+++ src/IfElse.java
@@ -6,14 +6,9 @@ public class IfElse {
             int y = 14;
             int z = 16;
 
-            if(x>=y && x>=z) {
-                System.out.println("X is the largest ");
-            } else(y>=x && y>=z){
-                System.out.println("Y is the largest");
-                }else(z>=x){System.out.println("z is the largest");
 
         }
 
     }
 
-}
+
